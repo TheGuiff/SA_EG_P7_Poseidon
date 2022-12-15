bb9dae31128063a8b97a63a733830888d87d9398
TheGuiff <ergu@mac.com>
2022-12-15 16:19:38 +0100

    feat:entity_bidlist

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/dal/entity/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/dal/entity/BidList.java
@@ -1,5 +1,7 @@
 package com.nnk.springboot.dal.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Required;
 
 import javax.persistence.*;
@@ -8,8 +10,88 @@ import javax.validation.constraints.NotBlank;
 import java.sql.Date;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "bidlist")
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer BidListId;
+
+    @Column
+    private String account;
+
+    @Column
+    private String type;
+
+    @Column(name = "bid_quantity")
+    Double bidQuantity;
+
+    @Column(name = "ask_quantity")
+    Double askQuantity;
+
+    @Column
+    Double bidQ;
+
+    @Column
+    Double ask;
+
+    @Column
+    private String benchmark;
+
+    @Column(name = "bid_list_date")
+    Timestamp bidListDate;
+
+    @Column
+    private String commentary;
+
+    @Column
+    private String security;
+
+    @Column
+    private String status;
+
+    @Column
+    private String trader;
+
+    @Column
+    private String book;
+
+    @Column(name="creation_name")
+    private String creationName;
+
+    @Column(name = "creation_date")
+    Timestamp creationDate;
+
+    @Column(name="revision_name")
+    private String revisionName;
+
+    @Column(name = "revision_date")
+    Timestamp revisionDate;
+
+    @Column(name="deal_name")
+    private String dealName;
+
+    @Column(name="deal_type")
+    private String dealType;
+
+    @Column(name="source_list_id")
+    private String sourceListId;
+
+    @Column
+    private String side;
+
+    public BidList (String accountIn,
+                    String typeIn,
+                    Double bidQuantityIn) {
+        this.setAccount(accountIn);
+        this.setType(typeIn);
+        this.setBidQuantity(bidQuantityIn);
+    }
+
+    public BidList() {
+
+    }
 }
