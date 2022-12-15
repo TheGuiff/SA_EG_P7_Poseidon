be04d203bbe8aebebe93f2917de332097824cd48
TheGuiff <ergu@mac.com>
2022-12-15 16:41:21 +0100

    feat:entity_rule_name

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/dal/entity/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/dal/entity/RuleName.java
@@ -1,11 +1,55 @@
 package com.nnk.springboot.dal.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "rulename")
 public class RuleName {
-    // TODO: Map columns in data table RULENAME with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private String json;
+
+    @Column
+    private String template;
+
+    @Column(name = "sql_str")
+    private String sqlStr;
+
+    @Column(name = "sql_part")
+    private String sqlPart;
+
+    public RuleName (String nameIn,
+                     String descriptionIn,
+                     String jsonIn,
+                     String templateIn,
+                     String sqlStrIn,
+                     String sqlPartIn) {
+        this.setName(nameIn);
+        this.setDescription(descriptionIn);
+        this.setJson(jsonIn);
+        this.setTemplate(templateIn);
+        this.setSqlStr(sqlStrIn);
+        this.setSqlPart(sqlPartIn);
+    }
+
+    public RuleName() {
+
+    }
 }
