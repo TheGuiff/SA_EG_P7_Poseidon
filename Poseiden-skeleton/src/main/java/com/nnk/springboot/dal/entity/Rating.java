eb13022e4f058df44fdb9cc6aa6bb705730b0b80
TheGuiff <ergu@mac.com>
2022-12-15 16:34:45 +0100

    feat:entity_rating

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/dal/entity/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/dal/entity/Rating.java
@@ -1,12 +1,46 @@
 package com.nnk.springboot.dal.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "moodys_rating")
+    private String moodysRating;
+
+    @Column(name = "sand_p_rating")
+    private String sandPRating;
+
+    @Column(name = "fitch_rating")
+    private String fitchRating;
+
+    @Column(name = "order_number")
+    private Integer orderNumber;
+
+    public Rating (String moodysRatingIn,
+                   String sandPRatingIn,
+                   String fitchRatingIn,
+                   Integer orderNumberIn) {
+        this.setMoodysRating(moodysRatingIn);
+        this.setSandPRating(sandPRatingIn);
+        this.setFitchRating(fitchRatingIn);
+        this.setOrderNumber(orderNumberIn);
+    }
+
+    public Rating() {
+
+    }
 }
