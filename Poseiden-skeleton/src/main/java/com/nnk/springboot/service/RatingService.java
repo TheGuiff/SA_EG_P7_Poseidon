bf8c6ca27e3971877531c247f68d37d8ad979cb4
TheGuiff <ergu@mac.com>
2022-12-16 11:14:15 +0100

    feat:service_crud_rating

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/RatingService.java
@@ -0,0 +1,33 @@
+package com.nnk.springboot.service;
+
+import com.nnk.springboot.dal.entity.Rating;
+import com.nnk.springboot.dal.repositories.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RatingService {
+
+    @Autowired
+    private RatingRepository ratingRepository;
+
+    public Optional<Rating> getRating (final Integer id) {
+        return ratingRepository.findById(id);
+    }
+
+    public Iterable<Rating> getRatings() {
+        return ratingRepository.findAll();
+    }
+
+    public  void deleteRating(final Integer id) {
+        ratingRepository.deleteById(id);
+    }
+
+    public Rating saveRating(Rating rating) {
+        Rating savedRating = ratingRepository.save(rating);
+        return savedRating;
+    }
+
+}
