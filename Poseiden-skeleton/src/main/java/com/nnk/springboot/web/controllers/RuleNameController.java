19c9a6a384ebb327e8b8fbbd3d40df78351c0991
TheGuiff <ergu@mac.com>
2023-01-04 11:16:18 +0100

    feat:curve_controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/web/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/web/controllers/RuleNameController.java
@@ -8,13 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -29,41 +33,73 @@ public class RuleNameController {
         log.info("List of rulenames");
         try {
             Iterable<RuleName> ruleNames = ruleNameService.getRuleNames();
-            model.addAttribute("rule", ruleNames);
+            List<RuleName> ruleNameList = new ArrayList<>();
+            ruleNames.forEach(ruleNameList::add);
+            model.addAttribute("rules", ruleNames);
         } catch (NoSuchElementException e) {
-            log.error("/trade/list : ",e.getMessage());
+            log.error("/ruleName/list : ",e.getMessage());
             model.addAttribute("Error", e.getMessage());
         }
         return "ruleName/list";
     }
 
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    public String addRuleForm(RuleName ruleName) {
         return "ruleName/add";
     }
 
     @PostMapping("/ruleName/validate")
     public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
+        log.info("Add ruleName");
+        if (!result.hasErrors()) {
+            ruleNameService.saveRuleName(ruleName);
+            model.addAttribute("rules", ruleNameService.getRuleNames());
+        } else {
+            List<ObjectError> errorList = result.getAllErrors();
+            log.error("/ruleName/validate/ : ",errorList.get(0));
+            model.addAttribute("error", errorList.get(0));
+        }
+        return "ruleName/list";
     }
 
     @GetMapping("/ruleName/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+        log.info("Update ruleName by id : " + String.valueOf(id));
+        try {
+            Optional<RuleName> ruleName = ruleNameService.getRuleName(id);
+            model.addAttribute("ruleName", ruleName.get());
+        } catch (NoSuchElementException e) {
+            log.error("/ruleName/update/" + String.valueOf(id) + " : ",e.getMessage());
+            model.addAttribute("Error", e.getMessage());
+        }
         return "ruleName/update";
     }
 
     @PostMapping("/ruleName/update/{id}")
     public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
-        return "redirect:/ruleName/list";
+        if (!result.hasErrors()) {
+            ruleName.setId(id);
+            ruleNameService.saveRuleName(ruleName);
+            model.addAttribute("rules", ruleNameService.getRuleNames());
+            return "redirect:/ruleName/list";
+        } else {
+            List<ObjectError> errorList = result.getAllErrors();
+            model.addAttribute("error", errorList.get(0));
+            return "ruleName/update";
+        }
     }
 
     @GetMapping("/ruleName/delete/{id}")
     public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
-        return "redirect:/ruleName/list";
+        log.info("Delete RuleName by id : " + String.valueOf(id));
+        try {
+            ruleNameService.deleteRuleName(id);
+            return "redirect:/ruleName/list";
+        } catch (NoSuchElementException e) {
+            log.error("/ruleName/delete/" + String.valueOf(id) + " : ",e.getMessage());
+            model.addAttribute("Error", e.getMessage());
+            return "ruleName/list";
+        }
     }
 }
